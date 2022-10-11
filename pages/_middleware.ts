e4962129763cf5875cf98d98a7a98e72c3b571e1
Ranielli <raniellimontagna@hotmail.com>
2022-10-11 16:57:34 -0300

    refactor: implementa `edge` middleware

---
+++ pages/_middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+
+export function middleware() {
+  // Store the response so we can modify its headers
+  const response = NextResponse.next();
+
+  // Set custom header
+  response.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate=59');
+
+  // Return response
+  return response;
+}
