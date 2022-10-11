84da02e5bad2396fdf717b033d92f1b9cb270f7c
Ranielli <raniellimontagna@hotmail.com>
2022-10-11 17:06:10 -0300

    refactor: ajusta `next.config` e remove middleware

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
     locales: ['pt-BR'],
     defaultLocale: 'pt-BR',
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
