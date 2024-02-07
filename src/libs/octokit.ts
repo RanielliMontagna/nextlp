27e774925f25349da8dce64268bded0ecf166013
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-02-07 08:42:06 -0300

    feat: :sparkles: adiciona endpoint de consulta para buscar repositórios próprios do github

---
+++ src/libs/octokit.ts
@@ -0,0 +1,18 @@
+import { Octokit } from 'octokit'
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
+async function getRepos() {
+  const response = await octokit.request('GET /users/RanielliMontagna/repos', {
+    username: 'RanielliMontagna',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  })
+
+  return response.data
+}
+
+export { getRepos }
